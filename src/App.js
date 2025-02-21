5e4696709e7a8d991dcaf9098bc55f7a0467e26d
tmcwilliam <71474441+tmcwilliam707@users.noreply.github.com>
2025-02-20 17:40:50 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import './CraterScene.css';
 const App = () => {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      {/* Pass the oid for the clooned-object instead of a filePath */}
-      <CraterScene oid="5cf97eb291154c349930081909c455a4" />
+      <CraterScene />
     </div>
   );
 };
